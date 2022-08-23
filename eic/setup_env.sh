6effeabb756d037e0eb1ee0f7a6f302d8d274c79
Fernando Torales Acosta <ftorales@borax2.llnl.gov>
2022-08-23 11:13:02 -0700

    EIC_DIR. Breaks login node setup without setting

--- eic/setup_env.sh
+++ eic/setup_env.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 source /opt/detector/setup.sh
-export LD_LIBRARY_PATH=$PWD/development/lib:$LD_LIBRARY_PATH
-export PATH=$PWD/development/bin:$PATH
-export DETECTOR_PATH=$PWD/athena
+
+printf "\n CURRENT WORKING DIRECTORY = {$PWD}"
+EIC_DIR=/p/lustre2/ftorales/generate_data/eic
+printf "\n EIC_DIR set to ${EIC_DIR}" 
+
+export LD_LIBRARY_PATH=${EIC_DIR}/development/lib:$LD_LIBRARY_PATH
+export PATH=${EIC_DIR}/development/bin:$PATH
+export DETECTOR_PATH=${EIC_DIR}/athena
 export JUGGLER_DETECTOR=athena
-export JUGGLER_INSTALL_PREFIX=$PWD/development
+export JUGGLER_INSTALL_PREFIX=${EIC_DIR}/development
 
 #HDF5                                                                                                                                                                    
-export HDF5_DIR=${PWD}/../to_hdf5/hdf5-1.8.22/hdf5                                                                                                                           
-export PATH=$PATH:${HDF5_DIR}/bin                                                                                                                                        
+export HDF5_DIR=${EIC_DIR}/../to_hdf5/hdf5-1.8.22/hdf5                                                                        
+export PATH=$PATH:${HDF5_DIR}/bin                                                                                             
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HDF5_DIR}/lib
-echo $HDF5_DIR                                                                                                                                                               
 
 #export HDF5_DIR=/clusterfs/ml4hep_nvme2/ftoralesacosta/generate_data/to_hdf5/hdf5-1.8.22/hdf5 
 #For Reference
