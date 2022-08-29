e236ac6fe8a6db370a998e4019065f767ad81a49
Fernando Torales Acosta <ftorales@borax2.llnl.gov>
2022-08-29 11:46:16 -0700

    Set EIC_DIR to PWD IFF not defined by environment

--- eic/setup_env.sh
+++ eic/setup_env.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 source /opt/detector/setup.sh
 
-EIC_DIR = $PWD
-#EIC_DIR=/p/lustre2/ftorales/generate_data/eic
-#EIC_DIR=/p/lustre2/marratia/generate_data/eic
-#EIC_DIR=/p/lustre1/dongwi1/analysis/hip/generate_data/eic
-#printf "EIC_DIR = ${EIC_DIR} \n"
+EIC_DIR=${EIC_DIR:-${PWD}} #If not defined, set it to pwd
+printf "EIC_DIR from setup_env.sh = ${EIC_DIR} \n"
 
 export LD_LIBRARY_PATH=${EIC_DIR}/development/lib:$LD_LIBRARY_PATH
 export PATH=${EIC_DIR}/development/bin:$PATH
@@ -17,5 +14,3 @@ export JUGGLER_INSTALL_PREFIX=${EIC_DIR}/development
 export HDF5_DIR=${EIC_DIR}/../to_hdf5/hdf5-1.8.22/hdf5                                                                        
 export PATH=$PATH:${HDF5_DIR}/bin                                                                                             
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HDF5_DIR}/lib
-
-#TODO: Reinstate ability to run without specifying $EIC_DIR
