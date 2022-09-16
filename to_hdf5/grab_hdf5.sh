2941f63adfed4d00fdb89319b5d79c276ebd87f2
Fernando T. Torales Acosta <ftoralesacosta@ml4hep1.lbl.gov>
2022-09-15 19:26:32 -0700

    hdf5 1.8->1.13

--- to_hdf5/grab_hdf5.sh
+++ to_hdf5/grab_hdf5.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.13/hdf5-1.13.1/src/hdf5-1.13.1.tar.gz
-# gzip -cd hdf5-1.13.1.tar.gz | tar xvf -
-# cd hdf5-1.13.1
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.22/src/hdf5-1.8.22.tar.gz
-gzip -cd hdf5-1.8.22.tar.gz | tar xvf -
-cd hdf5-1.8.22
+gzip -cd hdf5-1.13.2.tar.gz | tar xvf -
+cd hdf5-1.13.2
+
 ./configure --enable-cxx
 ldconfig
 #./config -flags for options. e.g. --enable-parallel
@@ -14,8 +11,6 @@ make install
 export HDF5_DIR=${PWD}/hdf5
 export PATH=$PATH:${HDF5_DIR}/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HDF5_DIR}/lib
-
-cd ../
 
 echo $HDF5_DIR
 echo "HDF5 Install Done"
